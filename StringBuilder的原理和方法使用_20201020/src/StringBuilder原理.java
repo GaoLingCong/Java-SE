51614db5bd60f687a0d2e17df01f9231262d4f21
GLC <you@example.com>
2020-10-20 23:08:26 +0800

    学会了DateForm类，fromat方法  parse方法

---
+++ StringBuilder的原理和方法使用_20201020/src/StringBuilder原理.java
@@ -0,0 +1,40 @@
+
+/*
+原理：
+String类
+字符串是常量；它们的值在创建之后不能更改。
+字符串的底层是一个被final修饰的数组，不能改变，是一个常量
+private final byte[] value;
+进行字符串的相加内存就会有多个字符串，占用空间多，效率低下
+String s = "a"+"b"+"c'="abc" 这样就有开辟多个内存
+
+
+StrinBulider类
+字符串缓冲区，可以提高字符串的操作效率（可以看成一个长度可以变化的字符串）
+底层也是一个数组，但是没有被final修饰，可以改变长度
+
+
+StringBuilder在内存中始终是一个数组，占用空间少，效率高
+如果超出StringBuilder的容量，会自动扩容
+
+
+
+java.lang.StringBuilder类:字符串缓冲区,可以提高字符串的效率
+构造方法:
+StringBuilder() 构造一个不带任何字符的字符串生成器，其初始容量为 16 个字符。
+ StringBuilder(String str) 构造一个字符串生成器，并初始化为指定的字符串内容。
+ */
+public class StringBuilder原理 {
+    public static void main(String[] args) {
+        //使用空参数构造方法
+        StringBuilder bu1  = new StringBuilder();
+        System.out.println("bu1:"+bu1);
+
+        //使用带字符串的构造方法
+        StringBuilder bu2 = new StringBuilder("abc");
+        System.out.println("bu2:"+bu2);
+
+
+    }
+
+}
---
+++ StringBuilder的原理和方法使用_20201020/src/StringBulider的常用方法.java
@@ -0,0 +1,40 @@
+/*
+    StringBuilder的常用方法:
+        public StringBuilder append(...)：添加任意类型数据的字符串形式，并返回当前对象自身。
+ */
+
+
+public class StringBulider的常用方法 {
+    public static void main(String[] args) {
+        //创建StringBuilder对象
+        StringBuilder bu = new StringBuilder();
+        //使用append方法往StringBuilder中添加数据
+        //append方法返回的是this，也就是说谁调用就返回谁，调用方法的对象du，this==du
+        StringBuilder bu2 = bu.append("abc");//把du的地址赋值给了bu2
+        System.out.println(bu);
+        System.out.println(bu2);
+        System.out.println(bu==bu2);//比较的是地址 true
+        System.out.println("========分界线===========");
+        //使用append方法无需接收返回值
+        bu.append("abc");
+        bu.append(1);
+        bu.append(1.232);
+        bu.append('高');
+        System.out.println(bu);
+        System.out.println("========分界线===========");
+        /*
+        * 链式编程：方法返回值是一个对象，可以继续调用方法
+        */
+        //例子
+        System.out.println("abc".toLowerCase().toLowerCase().toLowerCase());
+        //toLowerCase()方法是把大小转换小写
+        bu.append("abc").append(1).append(true).append('高');
+        System.out.println(bu);
+
+
+    }
+
+
+
+    }
+
